f7d59aaefcd9f04d2ecf59ad6015f4496519e6ba
Jake Huey <125854542+jhueyy@users.noreply.github.com>
2025-03-12 15:20:36 -0700

    added better comments

--- app/src/main/java/csc436/aitranslator/SAMPLE-OpenAIService.kt
+++ app/src/main/java/csc436/aitranslator/SAMPLE-OpenAIService.kt
@@ -5,7 +5,8 @@ import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface `SAMPLE-OpenAIService` {
-    // Replace KEY GOES HERE in Authorization with your actual OpenAI API key
+    // 1. Delete SAMPLE- from the filename
+    // 2. Replace KEY GOES HERE in Authorization with your actual OpenAI API key
     // the key will start with
     // Bearer sk-proj-...
     @Headers(
