03b37da6d2e08f6bcab2a17df2d7eed974267032
Jake Huey <125854542+jhueyy@users.noreply.github.com>
2025-03-12 13:54:23 -0700

    created a sample service for new users

---
+++ app/src/main/java/csc436/aitranslator/SAMPLE-OpenAIService.kt
@@ -0,0 +1,17 @@
+package csc436.aitranslator
+
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+interface `SAMPLE-OpenAIService` {
+    // Replace KEY GOES HERE in Authorization with your actual OpenAI API key
+    // the key will start with
+    // Bearer sk-proj-...
+    @Headers(
+        "Authorization: KEY GOES HERE",
+        "Content-Type: application/json"
+    )
+    @POST("chat/completions")
+    suspend fun translate(@Body request: TranslationRequest): OpenAIResponse
+}
